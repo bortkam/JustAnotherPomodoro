7a97b7a2bf66e3df5b9ca21c22e6288ff50474ae
Michał Bortka <michal.bortka@protonmail.com>
2023-07-17 23:19:07 +0200

    code cleanups

--- scripts/app.js
+++ scripts/app.js
@@ -1,68 +1,68 @@
 const wallpapers = [
-  {
-    "location":"media/images/wallpapers/aldrin-rachman-pradana-UEwMB7Z1pKQ-unsplash.jpg",
-    "author":"Aldrin Rachman Pradana",
-    "link":"https://unsplash.com/photos/UEwMB7Z1pKQ"
-  },
-  {
-    "location":"media/images/wallpapers/arthur-hinton-9Y1iOQ4rvtw-unsplash.jpg",
-    "author":"Arthur Hinton",
-    "link":"https://unsplash.com/photos/a-view-of-a-mountain-range-at-sunset-9Y1iOQ4rvtw"
-  },
-  {
-    "location":"media/images/wallpapers/jamo-images--WF1_mdidPU-unsplash.jpg",
-    "author":"Jamo Images",
-    "link":"https://unsplash.com/photos/-WF1_mdidPU"
-  },
-  {
-    "location":"media/images/wallpapers/jonas-verstuyft-fa73YB-Vono-unsplash.jpg",
-    "author":"Jonas Verstuyft",
-    "link":"https://unsplash.com/photos/fa73YB-Vono"
-  },
-  {
-    "location":"media/images/wallpapers/marijana-vasic-hKaoVrHXJXE-unsplash.jpg",
-    "author":"Marijana Vasic",
-    "link":"https://unsplash.com/photos/hKaoVrHXJXE",
-  },
-  {
-    "location":"media/images/wallpapers/max-bender-s4I1xpX_ny8-unsplash.jpg",
-    "author":"Max Bender",
-    "link":"https://unsplash.com/photos/s4I1xpX_ny8"
-  },
-  {
-    "location":"media/images/wallpapers/rebe-adelaida-zunQwMy5B6M-unsplash.jpg",
-    "author":"Rebe Adelaida",
-    "link":"https://unsplash.com/photos/zunQwMy5B6M"
-  },
-  {
-    "location":"media/images/wallpapers/sebastian-unrau-sp-p7uuT0tw-unsplash.jpg",
-    "author":"Sebastian Unrau",
-    "link":"https://unsplash.com/photos/sp-p7uuT0tw"
-  },
-  {
-    "location":"media/images/wallpapers/tobias-tullius-RhjVGxILcqE-unsplash.jpg",
-    "author":"Tobias Tullius",
-    "link":"https://unsplash.com/photos/RhjVGxILcqE"
-  },
-  {
-    "location":"media/images/wallpapers/vitalii-khodzinskyi-7WEi1kpLJYM-unsplash.jpg",
-    "author":"Vitalii Khodzinskyi",
-    "link":"https://unsplash.com/photos/7WEi1kpLJYM"
-  },
+	{
+		"location":"media/images/wallpapers/aldrin-rachman-pradana-UEwMB7Z1pKQ-unsplash.jpg",
+		"author":"Aldrin Rachman Pradana",
+		"link":"https://unsplash.com/photos/UEwMB7Z1pKQ"
+	},
+	{
+		"location":"media/images/wallpapers/arthur-hinton-9Y1iOQ4rvtw-unsplash.jpg",
+		"author":"Arthur Hinton",
+		"link":"https://unsplash.com/photos/a-view-of-a-mountain-range-at-sunset-9Y1iOQ4rvtw"
+	},
+	{
+		"location":"media/images/wallpapers/jamo-images--WF1_mdidPU-unsplash.jpg",
+		"author":"Jamo Images",
+		"link":"https://unsplash.com/photos/-WF1_mdidPU"
+	},
+	{
+		"location":"media/images/wallpapers/jonas-verstuyft-fa73YB-Vono-unsplash.jpg",
+		"author":"Jonas Verstuyft",
+		"link":"https://unsplash.com/photos/fa73YB-Vono"
+	},
+	{
+		"location":"media/images/wallpapers/marijana-vasic-hKaoVrHXJXE-unsplash.jpg",
+		"author":"Marijana Vasic",
+		"link":"https://unsplash.com/photos/hKaoVrHXJXE",
+	},
+	{
+		"location":"media/images/wallpapers/max-bender-s4I1xpX_ny8-unsplash.jpg",
+		"author":"Max Bender",
+		"link":"https://unsplash.com/photos/s4I1xpX_ny8"
+	},
+	{
+		"location":"media/images/wallpapers/rebe-adelaida-zunQwMy5B6M-unsplash.jpg",
+		"author":"Rebe Adelaida",
+		"link":"https://unsplash.com/photos/zunQwMy5B6M"
+	},
+	{
+		"location":"media/images/wallpapers/sebastian-unrau-sp-p7uuT0tw-unsplash.jpg",
+		"author":"Sebastian Unrau",
+		"link":"https://unsplash.com/photos/sp-p7uuT0tw"
+	},
+	{
+		"location":"media/images/wallpapers/tobias-tullius-RhjVGxILcqE-unsplash.jpg",
+		"author":"Tobias Tullius",
+		"link":"https://unsplash.com/photos/RhjVGxILcqE"
+	},
+	{
+		"location":"media/images/wallpapers/vitalii-khodzinskyi-7WEi1kpLJYM-unsplash.jpg",
+		"author":"Vitalii Khodzinskyi",
+		"link":"https://unsplash.com/photos/7WEi1kpLJYM"
+	},
 ]
 
 function toggleFullScreen() {
-    if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen();
-    } else if (document.exitFullscreen) {
-      document.exitFullscreen();
-    }
+	if (!document.fullscreenElement) {
+		document.documentElement.requestFullscreen();
+	} else if (document.exitFullscreen) {
+		document.exitFullscreen();
+	}
 }
 
 function randomizeWallpaper() {
-  let wallpapersIndex = Math.floor(Math.random() * wallpapers.length);
-  let img = wallpapers[wallpapersIndex];
-  document.querySelector("html").style.backgroundImage = "url("+img.location+")";
-  document.getElementById("wallpaperAuthor").innerHTML = img.author;
-  document.getElementById("wallpaperLink").href = img.link;
+	let wallpapersIndex = Math.floor(Math.random() * wallpapers.length);
+	let img = wallpapers[wallpapersIndex];
+	document.querySelector("html").style.backgroundImage = "url("+img.location+")";
+	document.getElementById("wallpaperAuthor").innerHTML = img.author;
+	document.getElementById("wallpaperLink").href = img.link;
 }
--- scripts/pomodoro.js
+++ scripts/pomodoro.js
@@ -1,153 +1,157 @@
-const minuteDurationInSeconds = 60;
 var time,
-    isTimerClear = true,
-    mode = "work",
-    isWorking = false,
-    counterInterval,
-    timeToGo;
-    audio = new Audio('media/sounds/analog-alarm-clock.wav');
+	isTimerClear = true,
+	mode = "work",
+	isWorking = false,
+	counterInterval,
+	timeToGo;
+	audio = new Audio('media/sounds/analog-alarm-clock.wav');
 
 window.addEventListener('beforeunload', function (e) {
-    if (isWorking) {
-        // display alert when exiting website
-        e.preventDefault();
-        e.returnValue = '';
-    }
+	if (isWorking) {
+		// display alert when exiting website while clock is counting
+		e.preventDefault();
+		e.returnValue = '';
+	}
 });
 
 function getTimeInSeconds() {
-    // gets time in minutes from settings panel, and returns it as seconds
-    if (mode === "work") {
-        return parseInt(document.getElementById("workTime").innerHTML)*60;
-    }
-    return parseInt(document.getElementById("restTime").innerHTML)*60;
+	// gets time in minutes from settings panel, and returns it as seconds
+	if (mode === "work") {
+		return parseInt(document.getElementById("workTime").innerHTML)*60;
+	}
+	return parseInt(document.getElementById("restTime").innerHTML)*60;
 }
 
 function changeTimeFormat(timeInSeconds) { 
-    // receieves time in ss format and returns in mm:ss format
-    let minutes = Math.floor(timeInSeconds / 60);
-    let extraSeconds = timeInSeconds % 60;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    extraSeconds = extraSeconds < 10 ? "0" + extraSeconds : extraSeconds;
-    return minutes+":"+extraSeconds;
+	// receieves time in ss format and returns in mm:ss format
+	let minutes = Math.floor(timeInSeconds / 60);
+	let extraSeconds = timeInSeconds % 60;
+	minutes = minutes < 10 ? "0" + minutes : minutes;
+	extraSeconds = extraSeconds < 10 ? "0" + extraSeconds : extraSeconds;
+	return minutes+":"+extraSeconds;
 }
 
 function displayTime() {
-    let timeInMinutes = document.getElementById("pomodoroTimer").innerHTML=changeTimeFormat(timeToGo);
-    document.title = timeInMinutes+" | "+mode;
+	let timeInMinutes = document.getElementById("pomodoroTimer").innerHTML
+		= changeTimeFormat(timeToGo);
+	document.title = timeInMinutes+" | "+mode;
 }
 
 function countdown() {
-    counterInterval = setInterval(() => {
-        timeToGo--;
-        displayTime();
-        if (timeToGo == 0) {
-            audio.play();
-            clearInterval(counterInterval);
-            if (mode === "work") {
-                rest();
-            } else {
-                work();
-            }
-            start();
-        }
-    }, 1000);
+	counterInterval = setInterval(() => {
+		timeToGo--;
+		displayTime();
+		if (timeToGo != 0) {
+			return 0;
+		}
+		audio.play();
+		clearInterval(counterInterval);
+		if (mode === "work") {
+			rest();
+		} else {
+			work();
+		}
+		start();
+	}, 1000);
 }
 
 function pause() {
-    clearInterval(counterInterval);
+	clearInterval(counterInterval);
 }
 
 function reset() {
-    if (!isTimerClear) {
-        clearInterval(counterInterval);
-        document.getElementById("startButton").innerHTML="<img src=\"media/images//icons/play_arrow.svg\" alt=\"start\">";
-        document.title = "JustAnotherPomodoro";
-    }
-    isWorking = false;
-    
-    if (mode === "work") {
-        document.getElementById("pomodoroTimer").innerHTML=changeTimeFormat(getTimeInSeconds());
-    } else {
-        document.getElementById("pomodoroTimer").innerHTML=changeTimeFormat(getTimeInSeconds());
-    }
-    isTimerClear = true;
+	if (!isTimerClear) {
+		pause();
+		document.getElementById("startButtonImage").src =
+			"media/images/icons/play_arrow.svg"
+		document.title = "JustAnotherPomodoro";
+	}
+	isWorking = false;
+		
+	if (mode === "work") {
+		document.getElementById("pomodoroTimer").innerHTML = 
+			changeTimeFormat(getTimeInSeconds());
+	} else {
+		document.getElementById("pomodoroTimer").innerHTML = 
+			changeTimeFormat(getTimeInSeconds());
+	}
+	isTimerClear = true;
 }
 
 function start() {
-    if (isTimerClear === true) {
-        timeToGo = getTimeInSeconds();
-    }
+	if (isTimerClear === true) {
+		timeToGo = getTimeInSeconds();
+	}
 
-    if (isWorking) {
-        isWorking = false;
-        document.getElementById("startButton").innerHTML="<img src=\"media/images/icons/play_arrow.svg\" alt=\"start\">";
-        pause();
-    } else {
-        isWorking = true;
-        document.getElementById("startButton").innerHTML="<img src=\"media/images/icons/pause.svg\" alt=\"stop\">";
-        //if (isTimerClear) {
-            isTimerClear = false;
-        //}
-        countdown();
-    }
+	if (isWorking) {
+		isWorking = false;
+		document.getElementById("startButtonImage").src = 
+			"media/images/icons/play_arrow.svg";
+		pause();
+	} else {
+		isWorking = true;
+		document.getElementById("startButtonImage").src = 
+			"media/images/icons/pause.svg";
+		isTimerClear = false;
+		countdown();
+	}
 }
 
 function work() {
-    if (mode !== "work") {
-        mode = "work";
-        document.getElementById("workButton").style.backgroundColor = "#cfcfcf";
-        document.getElementById("restButton").style.backgroundColor = "#fafafa";
-        reset();
-    } 
+	if (mode !== "work") {
+		mode = "work";
+		document.getElementById("workButton").style.backgroundColor = "#cfcfcf";
+		document.getElementById("restButton").style.backgroundColor = "#fafafa";
+		reset();
+	} 
 }
 
 function rest() {
-    if (mode !== "rest") {
-        mode = "rest";
-        document.getElementById("workButton").style.backgroundColor = "#fafafa";
-        document.getElementById("restButton").style.backgroundColor = "#cfcfcf";
-        reset();
-    }
+	if (mode !== "rest") {
+		mode = "rest";
+		document.getElementById("workButton").style.backgroundColor = "#fafafa";
+		document.getElementById("restButton").style.backgroundColor = "#cfcfcf";
+		reset();
+	}
 }
 
 function IncrementDecrement(whatToIncrementDecrement, incrementOrDecrement) {
-    let numberToChange;
+	let numberToChange;
 
-    if (whatToIncrementDecrement === "workTime") {
-        numberToChange = parseInt(document.getElementById("workTime").innerHTML);
-    } else {
-        numberToChange = parseInt(document.getElementById("restTime").innerHTML);
-    }
+	if (whatToIncrementDecrement === "workTime") {
+		numberToChange = parseInt(document.getElementById("workTime").innerHTML);
+	} else {
+		numberToChange = parseInt(document.getElementById("restTime").innerHTML);
+	}
 
-    if (incrementOrDecrement === "increment") {
-        if (numberToChange <= 58) {
-            numberToChange++;
-        }
-    } else {
-        if (numberToChange >= 2) {
-            numberToChange--;
-        }
-    }
-    document.getElementById(whatToIncrementDecrement).innerHTML=numberToChange;
-    if (isWorking === false) {
-        reset();
-    }
+	if (incrementOrDecrement === "increment") {
+		if (numberToChange <= 58) {
+			numberToChange++;
+		}
+	} else {
+		if (numberToChange >= 2) {
+			numberToChange--;
+		}
+	}
+	document.getElementById(whatToIncrementDecrement).innerHTML = numberToChange;
+	if (!isWorking) {
+		reset();
+	}
 }
 
 function buttonsInSettings(clickedButtonID) {
-    switch (clickedButtonID) {
-        case 'incrementWork':
-            IncrementDecrement(document.getElementById("workTime").id,"increment");
-        break;
-        case 'decrementWork':
-            IncrementDecrement(document.getElementById("workTime").id,"decrement");
-        break;
-        case 'incrementRest':
-            IncrementDecrement(document.getElementById("restTime").id,"increment");
-        break;
-        case 'decrementRest':
-            IncrementDecrement(document.getElementById("restTime").id,"decrement");
-        break;
-      }
+	switch (clickedButtonID) {
+		case 'incrementWork':
+			IncrementDecrement(document.getElementById("workTime").id,"increment");
+			break;
+		case 'decrementWork':
+			IncrementDecrement(document.getElementById("workTime").id,"decrement");
+			break;
+		case 'incrementRest':
+			IncrementDecrement(document.getElementById("restTime").id,"increment");
+			break;
+		case 'decrementRest':
+			IncrementDecrement(document.getElementById("restTime").id,"decrement");
+			break;
+	}
 }
